ea93a045ac87f500fd0bf2c513147edc168b6ca1
midonour <mnw2637@gmail.com>
2026-04-06 19:32:28 +0200

    update sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,12 +1,7 @@
+import { p } from "motion/react-client";
+
 export default function sitemap() {
   const base = "https://mohamed-nour.vercel.app";
 
-  return [
-    { url: base, lastModified: new Date() },
-    { url: `${base}#About`, lastModified: new Date() },
-    { url: `${base}#Work`, lastModified: new Date() },
-    { url: `${base}#Contact`, lastModified: new Date() },
-    { url: `${base}#Skills`, lastModified: new Date() },
-    { url: `${base}#Home`, lastModified: new Date() },
-  ];
+  return [{ url: base, lastModified: new Date(), priority: 1 }];
 }
