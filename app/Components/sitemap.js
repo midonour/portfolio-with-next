b03639093a593722b9204648bea392ccf2c5793e
midonour <mnw2637@gmail.com>
2026-04-06 10:47:32 +0200

    sitemap and google console file

--- app/Components/sitemap.js
+++ app/Components/sitemap.js
@@ -1,25 +1,12 @@
 export default function sitemap() {
-  const baseUrl = "https://mohamed-nour.vercel.app/";
+  const base = "https://mohamed-nour.vercel.app";
+
   return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/#projects`,
-      lastModified: new Date(),
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#about`,
-      lastModified: new Date(),
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#contact`,
-      lastModified: new Date(),
-      priority: 0.8,
-    },
+    { url: base, lastModified: new Date() },
+    { url: `${base}#About`, lastModified: new Date() },
+    { url: `${base}#Work`, lastModified: new Date() },
+    { url: `${base}#Contact`, lastModified: new Date() },
+    { url: `${base}#Skills`, lastModified: new Date() },
+    { url: `${base}#Home`, lastModified: new Date() },
   ];
 }
